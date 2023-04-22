6f48e0de2ce4fa562a2d2067f799a96c70151bcb
misga <misga@MISTHERG>
2023-04-22 12:12:55 -0400

    Agregar archivos de proyecto.

---
+++ CRUDLABPROG4/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace CRUDLABPROG4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxMatricula = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxNombre = new System.Windows.Forms.TextBox();
+            this.button1Guardar = new System.Windows.Forms.Button();
+            this.button2Consulta = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(47, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(228, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "TAREA LAB PROGRAMACION 4";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(47, 83);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Matricula";
+            // 
+            // textBoxMatricula
+            // 
+            this.textBoxMatricula.Location = new System.Drawing.Point(103, 83);
+            this.textBoxMatricula.Name = "textBoxMatricula";
+            this.textBoxMatricula.Size = new System.Drawing.Size(140, 20);
+            this.textBoxMatricula.TabIndex = 2;
+            this.textBoxMatricula.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(47, 128);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Nombre";
+            // 
+            // textBoxNombre
+            // 
+            this.textBoxNombre.Location = new System.Drawing.Point(103, 128);
+            this.textBoxNombre.Name = "textBoxNombre";
+            this.textBoxNombre.Size = new System.Drawing.Size(140, 20);
+            this.textBoxNombre.TabIndex = 4;
+            // 
+            // button1Guardar
+            // 
+            this.button1Guardar.Location = new System.Drawing.Point(50, 189);
+            this.button1Guardar.Name = "button1Guardar";
+            this.button1Guardar.Size = new System.Drawing.Size(97, 55);
+            this.button1Guardar.TabIndex = 5;
+            this.button1Guardar.Text = "Guardar";
+            this.button1Guardar.UseVisualStyleBackColor = true;
+            this.button1Guardar.Click += new System.EventHandler(this.button1Guardar_Click);
+            // 
+            // button2Consulta
+            // 
+            this.button2Consulta.Location = new System.Drawing.Point(155, 189);
+            this.button2Consulta.Name = "button2Consulta";
+            this.button2Consulta.Size = new System.Drawing.Size(120, 55);
+            this.button2Consulta.TabIndex = 6;
+            this.button2Consulta.Text = "Consultar";
+            this.button2Consulta.UseVisualStyleBackColor = true;
+            this.button2Consulta.Click += new System.EventHandler(this.button2Consulta_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(321, 274);
+            this.Controls.Add(this.button2Consulta);
+            this.Controls.Add(this.button1Guardar);
+            this.Controls.Add(this.textBoxNombre);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxMatricula);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxMatricula;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxNombre;
+        private System.Windows.Forms.Button button1Guardar;
+        private System.Windows.Forms.Button button2Consulta;
+    }
+}
+
---
+++ CRUDLABPROG4/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using System.Reflection.Emit;
+
+namespace CRUDLABPROG4
+{
+    public partial class Form1 : Form
+    {
+   
+        public Form1()
+        {
+            InitializeComponent();
+         
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1Guardar_Click(object sender, EventArgs e)
+        {
+            SqlConnection conexion = new SqlConnection("server=MISTHERG ; database=LabProg ; integrated security = true");
+            conexion.Open();
+            string matricula = textBoxMatricula.Text;
+            string nombre = textBoxNombre.Text;
+            string cadena = "insert into Crud(matricula,nombre) values (" + matricula + ",'" + nombre + "')";
+            SqlCommand comando = new SqlCommand(cadena, conexion);
+            comando.ExecuteNonQuery();
+            MessageBox.Show("Los datos se guardaron correctamente");
+            textBoxMatricula.Text = "";
+            textBoxNombre.Text = "";
+            conexion.Close();
+        }
+
+        private void button2Consulta_Click(object sender, EventArgs e)
+        {
+            SqlConnection conexion = new SqlConnection("server=MISTHERG ; database=LabProg ; integrated security = true");
+            conexion.Open();
+            string matricula = textBoxMatricula.Text;
+            string cadena = "select nombre from Crud where matricula=" + matricula;
+            SqlCommand comando = new SqlCommand(cadena, conexion);
+            SqlDataReader registro = comando.ExecuteReader();
+            if (registro.Read())
+            {
+                textBoxNombre.Text = registro["nombre"].ToString();
+                
+            }
+            else
+                MessageBox.Show("No existe un estudiante con la matricula ingresada");
+            conexion.Close();
+        }
+    }
+}
